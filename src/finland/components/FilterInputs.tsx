7716b67d9e02ca973afb37bd62f82c57bf722945
v-teemu.tuovinen <v-teemu.tuovinen@lingsoft.fi>
2023-11-01 15:39:45 +0200

    Use react-searchable-select in filter selects

--- src/finland/components/FilterInputs.tsx
+++ src/finland/components/FilterInputs.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { KeyValuePair, filterKeys, readable, EventKey } from "../finlandUtils";
 import { FacetData } from "../OpenSearchAnalyticsContext";
+import SelectSearch from "react-select-search";
+import { fuzzySearch } from "react-select-search";
 
 type FilterInputsProps = {
   toggleFilter: (selection: KeyValuePair) => void;
@@ -21,32 +23,34 @@ export function FilterInputs({
 }: FilterInputsProps) {
   return (
     <div className="input-wrapper">
-      {filterKeys.map((key) => (
-        <div key={key} className="flex-col">
-          <label htmlFor={key}>{readable(key)}</label>
-          <select
-            className="form-control"
-            name={key}
-            id={key}
-            onChange={(event) => {
-              toggleFilter({
-                key: key as EventKey,
-                value: event.target.value,
-              });
-              event.target.value = "";
-            }}
-          >
-            <option key="all" value="">
-              – rajaa –
-            </option>
-            {facetData[key]?.buckets.map((item) => (
-              <option key={item.key} value={item.key}>
-                {item.key}
-              </option>
-            ))}
-          </select>
-        </div>
-      ))}
+      {filterKeys.map((key) => {
+        const options =
+          facetData[key]?.buckets.map((item) => ({
+            value: item.key,
+            name: item.key,
+          })) || [];
+        const label = readable(key);
+        return (
+          <div key={key} className="flex-col">
+            <label htmlFor={key}>{label}</label>
+            <SelectSearch
+              options={options}
+              search
+              placeholder="Rajaa.."
+              onChange={(item) => {
+                if (typeof item === "string") {
+                  toggleFilter({
+                    key: key as EventKey,
+                    value: item,
+                  });
+                }
+              }}
+              filterOptions={fuzzySearch}
+            />
+          </div>
+        );
+      })}
+
       <div className="flex-col">
         <label htmlFor="start-date">Alkaen</label>
         <input
