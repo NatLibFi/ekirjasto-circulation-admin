21559e0e8f30c4fe1a634fcf534c3b478f05b891
v-teemu.tuovinen <v-teemu.tuovinen@lingsoft.fi>
2023-11-01 12:46:22 +0200

    Use E-Kirjasto logo and change root title

--- src/utils/title.ts
+++ src/utils/title.ts
@@ -1,4 +1,4 @@
-const ROOT_TITLE = "Palace Collection Manager";
+export const ROOT_TITLE = "E-Kirjasto Admin";
 
 export default (...subtitle: string[]): string => {
   return [ROOT_TITLE, ...subtitle].filter((part) => !!part).join(" - ");
