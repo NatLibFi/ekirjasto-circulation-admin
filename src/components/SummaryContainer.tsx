2c8adc78e13f825121b5ef17dafc4b8afd9ebdb8
Kaisa Kuivalahti <kaisa.kuivalahti@helsinki.fi>
2026-09-17 16:28:19 +0300

    Comment summary

--- src/components/SummaryContainer.tsx
+++ src/components/SummaryContainer.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 
-/** Renders the complete summary supplied by the OPDS book data. */
+/**
+ * Renders the complete summary supplied by the OPDS book data.
+ * The feed summary is expected to contain the markup provided by the source.
+ */
 export default function SummaryContainer({
   summary,
   language,
